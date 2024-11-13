f0430a8d3be89a471cca8814c5e43ca04811d14c
Matthew Edwards <wste993l@students.codeinstitute.net>
2024-11-13 10:53:16 +0000

    add delay on restartGame, change buttons with posters as event Listeners.

--- assets/js/quiz-script.js
+++ assets/js/quiz-script.js
@@ -1,303 +1,303 @@
 const test = [{
-    name: "Avatar",
-    value: 2923706026,
-    img: "avatar.jpg",
-    date: 2009,
-},
-{
-    name: "Avengers: Endgame",
-    value: 2799439100,
-    img: "avengers-endgame.jpg",
-    date: 2019,
-},
-{
-    name: "Avatar: The Way of Water",
-    value: 2320250281,
-    img: "avatar-way-of-water.jpg",
-    date: 2022,
-},
-{
-    name: "Titanic",
-    value: 2264750694,
-    img: "titanic.jpg",
-    date: 1997,
-},
-{
-    name: "Star Wars: Episode VII - The Force Awakens",
-    value: 2071310218,
-    img: "force-awakens.jpg",
-    date: 2015,
-},
-{
-    name: "Avengers: Infinity War",
-    value: 2052415039,
-    img: "infinity-war.jpg",
-    date: 2018,
-},
-{
-    name: "Spider-Man: No Way Home",
-    value: 1952723719,
-    img: "no-way-home.jpg",
-    date: 2021,
-},
-{
-    name: "Inside Out 2",
-    value: 1697092880,
-    img: "inside-out-2.jpg",
-    date: 2024,
-},
-{
-    name: "Jurassic World",
-    value: 1671537444,
-    img: "jurassic-world.jpg",
-    date: 2015,
-},
-{
-    name: "The Lion King",
-    value: 1662020819,
-    img: "lion-king.jpg",
-    date: 2019,
-},
-{
-    name: "The Avengers",
-    value: 1520538536,
-    img: "avengers.jpg",
-    date: 2012,
-},
-{
-    name: "Furious 7",
-    value: 1515342457,
-    img: "furious-7.jpg",
-    date: 2015,
-},
-{
-    name: "Top Gun: Maverick",
-    value: 1495696292,
-    img: "top-gun-maverick.jpg",
-    date: 2022,
-},
-{
-    name: "Frozen 2",
-    value: 1453683476,
-    img: "frozen-2.jpg",
-    date: 2019,
-},
-{
-    name: "Barbie",
-    value: 1446938421,
-    img: "barbie.jpg",
-    date: 2023,
-},
-{
-    name: "Avengers: Age of Ultron",
-    value: 1405018048,
-    img: "age-of-ultron.jpg",
-    date: 2015,
-},
-{
-    name: "The Super Mario Bros. Movie",
-    value: 1362566989,
-    img: "super-mario.jpg",
-    date: 2023,
-},
-{
-    name: "Black Panther",
-    value: 1349926083,
-    img: "black-panther.jpg",
-    date: 2018,
-},
-{
-    name: "Harry Potter and the Deathly Hallows: Part 2",
-    value: 1342480797,
-    img: "deathly-hallows-2.jpg",
-    date: 2011,
-},
-{
-    name: "Deadpool & Wolverine",
-    value: 1337799474,
-    img: "deadpool-wolverine.jpg",
-    date: 2024,
-},
-{
-    name: "Star Wars: Episode VIII - The Last Jedi",
-    value: 1334407706,
-    img: "last-jedi.jpg",
-    date: 2017,
-},
-{
-    name: "Jurassic World: Fallen Kingdom",
-    value: 1310469037,
-    img: "fallen-kingdom.jpg",
-    date: 2018,
-},
-{
-    name: "Frozen",
-    value: 1306450154,
-    img: "frozen.jpg",
-    date: 2013,
-},
-{
-    name: "Iron Man 3",
-    value: 1266152644,
-    img: "iron-man-3.jpg",
-    date: 2013,
-},
-{
-    name: "Beauty and the Beast",
-    value: 1266115964,
-    img: "beauty-beast.jpg",
-    date: 2017,
-},
-{
-    name: "Incredibles 2",
-    value: 1243225667,
-    img: "incredibles-2.jpg",
-    date: 2018,
-},
-{
-    name: "The Fate of the Furious",
-    value: 1236009236,
-    img: "fate-of-the-furious.jpg",
-    date: 2017,
-},
-{
-    name: "Minions",
-    value: 1159457503,
-    img: "minions.jpg",
-    date: 2015,
-},
-{
-    name: "Captain America: Civil War",
-    value: 1155046416,
-    img: "captain-america-civil-war.jpg",
-    date: 2016,
-},
-{
-    name: "Aquaman",
-    value: 1152028393,
-    img: "aquaman.jpg",
-    date: 2018,
-},
-{
-    name: "The Lord of the Rings: The Return of the King",
-    value: 1138027091,
-    img: "lotr-return-of-the-king.jpg",
-    date: 2003,
-},
-{
-    name: "Spider-Man: Far from Home",
-    value: 1132705055,
-    img: "spider-man-far-from-home.jpg",
-    date: 2019,
-},
-{
-    name: "Captain Marvel",
-    value: 1131416446,
-    img: "captain-marvel.jpg",
-    date: 2019,
-},
-{
-    name: "Transformers: Dark of the Moon",
-    value: 1123794079,
-    img: "transformers-dark-of-the-moon.jpg",
-    date: 2011,
-},
-{
-    name: "The Dark Knight Rises",
-    value: 1114975066,
-    img: "dark-knight-rises.jpg",
-    date: 2012,
-},
-{
-    name: "Skyfall",
-    value: 1108594137,
-    img: "skyfall.jpg",
-    date: 2012,
-},
-{
-    name: "Transformers: Age of Extinction",
-    value: 1105261713,
-    img: "transformers-age-of-extinction.jpg",
-    date: 2014,
-},
-{
-    name: "Jurassic Park",
-    value: 1104379926,
-    img: "jurassic-park.jpg",
-    date: 1993,
-},
-{
-    name: "Joker",
-    value: 1078958629,
-    img: "joker.jpg",
-    date: 2019,
-},
-{
-    name: "Star Wars: Episode IX - The Rise of Skywalker",
-    value: 1077022372,
-    img: "rise-of-skywalker.jpg",
-    date: 2019,
-},
-{
-    name: "Toy Story 4",
-    value: 1073841394,
-    img: "toy-story-4.jpg",
-    date: 2019,
-},
-{
-    name: "Toy Story 3",
-    value: 1067316101,
-    img: "toy-story-3.jpg",
-    date: 2010,
-},
-{
-    name: "Pirates of the Caribbean: Dead Man's Chest",
-    value: 1066179747,
-    img: "potc-deadmans-chest.jpg",
-    date: 2006,
-},
-{
-    name: "Rogue One: A Star Wars Story",
-    value: 1058684742,
-    img: "rogue-one.jpg",
-    date: 2016,
-},
-{
-    name: "Aladdin",
-    value: 1054304000,
-    img: "aladdin.jpg",
-    date: 2019,
-},
-{
-    name: "Pirates of the Caribbean: On Stranger Tides",
-    value: 1046721266,
-    img: "potc-on-stranger-tides.jpg",
-    date: 2011,
-},
-{
-    name: "Star Wars: Episode I - The Phantom Menace",
-    value: 1046515409,
-    img: "phantom-menace.jpg",
-    date: 1999,
-},
-{
-    name: "Despicable Me 3",
-    value: 1034800131,
-    img: "despicable-me-3.jpg",
-    date: 2017,
-},
-{
-    name: "Finding Dory",
-    value: 1029266989,
-    img: "finding-dory.jpg",
-    date: 2016,
-},
-{
-    name: "Harry Potter and the Philosopher's Stone",
-    value: 1026407721,
-    img: "philosophers-stone.jpg",
-    date: 2001,
-}
+        name: "Avatar",
+        value: 2923706026,
+        img: "avatar.jpg",
+        date: 2009,
+    },
+    {
+        name: "Avengers: Endgame",
+        value: 2799439100,
+        img: "avengers-endgame.jpg",
+        date: 2019,
+    },
+    {
+        name: "Avatar: The Way of Water",
+        value: 2320250281,
+        img: "avatar-way-of-water.jpg",
+        date: 2022,
+    },
+    {
+        name: "Titanic",
+        value: 2264750694,
+        img: "titanic.jpg",
+        date: 1997,
+    },
+    {
+        name: "Star Wars: Episode VII - The Force Awakens",
+        value: 2071310218,
+        img: "force-awakens.jpg",
+        date: 2015,
+    },
+    {
+        name: "Avengers: Infinity War",
+        value: 2052415039,
+        img: "infinity-war.jpg",
+        date: 2018,
+    },
+    {
+        name: "Spider-Man: No Way Home",
+        value: 1952723719,
+        img: "no-way-home.jpg",
+        date: 2021,
+    },
+    {
+        name: "Inside Out 2",
+        value: 1697092880,
+        img: "inside-out-2.jpg",
+        date: 2024,
+    },
+    {
+        name: "Jurassic World",
+        value: 1671537444,
+        img: "jurassic-world.jpg",
+        date: 2015,
+    },
+    {
+        name: "The Lion King",
+        value: 1662020819,
+        img: "lion-king.jpg",
+        date: 2019,
+    },
+    {
+        name: "The Avengers",
+        value: 1520538536,
+        img: "avengers.jpg",
+        date: 2012,
+    },
+    {
+        name: "Furious 7",
+        value: 1515342457,
+        img: "furious-7.jpg",
+        date: 2015,
+    },
+    {
+        name: "Top Gun: Maverick",
+        value: 1495696292,
+        img: "top-gun-maverick.jpg",
+        date: 2022,
+    },
+    {
+        name: "Frozen 2",
+        value: 1453683476,
+        img: "frozen-2.jpg",
+        date: 2019,
+    },
+    {
+        name: "Barbie",
+        value: 1446938421,
+        img: "barbie.jpg",
+        date: 2023,
+    },
+    {
+        name: "Avengers: Age of Ultron",
+        value: 1405018048,
+        img: "age-of-ultron.jpg",
+        date: 2015,
+    },
+    {
+        name: "The Super Mario Bros. Movie",
+        value: 1362566989,
+        img: "super-mario.jpg",
+        date: 2023,
+    },
+    {
+        name: "Black Panther",
+        value: 1349926083,
+        img: "black-panther.jpg",
+        date: 2018,
+    },
+    {
+        name: "Harry Potter and the Deathly Hallows: Part 2",
+        value: 1342480797,
+        img: "deathly-hallows-2.jpg",
+        date: 2011,
+    },
+    {
+        name: "Deadpool & Wolverine",
+        value: 1337799474,
+        img: "deadpool-wolverine.jpg",
+        date: 2024,
+    },
+    {
+        name: "Star Wars: Episode VIII - The Last Jedi",
+        value: 1334407706,
+        img: "last-jedi.jpg",
+        date: 2017,
+    },
+    {
+        name: "Jurassic World: Fallen Kingdom",
+        value: 1310469037,
+        img: "fallen-kingdom.jpg",
+        date: 2018,
+    },
+    {
+        name: "Frozen",
+        value: 1306450154,
+        img: "frozen.jpg",
+        date: 2013,
+    },
+    {
+        name: "Iron Man 3",
+        value: 1266152644,
+        img: "iron-man-3.jpg",
+        date: 2013,
+    },
+    {
+        name: "Beauty and the Beast",
+        value: 1266115964,
+        img: "beauty-beast.jpg",
+        date: 2017,
+    },
+    {
+        name: "Incredibles 2",
+        value: 1243225667,
+        img: "incredibles-2.jpg",
+        date: 2018,
+    },
+    {
+        name: "The Fate of the Furious",
+        value: 1236009236,
+        img: "fate-of-the-furious.jpg",
+        date: 2017,
+    },
+    {
+        name: "Minions",
+        value: 1159457503,
+        img: "minions.jpg",
+        date: 2015,
+    },
+    {
+        name: "Captain America: Civil War",
+        value: 1155046416,
+        img: "captain-america-civil-war.jpg",
+        date: 2016,
+    },
+    {
+        name: "Aquaman",
+        value: 1152028393,
+        img: "aquaman.jpg",
+        date: 2018,
+    },
+    {
+        name: "The Lord of the Rings: The Return of the King",
+        value: 1138027091,
+        img: "lotr-return-of-the-king.jpg",
+        date: 2003,
+    },
+    {
+        name: "Spider-Man: Far from Home",
+        value: 1132705055,
+        img: "spider-man-far-from-home.jpg",
+        date: 2019,
+    },
+    {
+        name: "Captain Marvel",
+        value: 1131416446,
+        img: "captain-marvel.jpg",
+        date: 2019,
+    },
+    {
+        name: "Transformers: Dark of the Moon",
+        value: 1123794079,
+        img: "transformers-dark-of-the-moon.jpg",
+        date: 2011,
+    },
+    {
+        name: "The Dark Knight Rises",
+        value: 1114975066,
+        img: "dark-knight-rises.jpg",
+        date: 2012,
+    },
+    {
+        name: "Skyfall",
+        value: 1108594137,
+        img: "skyfall.jpg",
+        date: 2012,
+    },
+    {
+        name: "Transformers: Age of Extinction",
+        value: 1105261713,
+        img: "transformers-age-of-extinction.jpg",
+        date: 2014,
+    },
+    {
+        name: "Jurassic Park",
+        value: 1104379926,
+        img: "jurassic-park.jpg",
+        date: 1993,
+    },
+    {
+        name: "Joker",
+        value: 1078958629,
+        img: "joker.jpg",
+        date: 2019,
+    },
+    {
+        name: "Star Wars: Episode IX - The Rise of Skywalker",
+        value: 1077022372,
+        img: "rise-of-skywalker.jpg",
+        date: 2019,
+    },
+    {
+        name: "Toy Story 4",
+        value: 1073841394,
+        img: "toy-story-4.jpg",
+        date: 2019,
+    },
+    {
+        name: "Toy Story 3",
+        value: 1067316101,
+        img: "toy-story-3.jpg",
+        date: 2010,
+    },
+    {
+        name: "Pirates of the Caribbean: Dead Man's Chest",
+        value: 1066179747,
+        img: "potc-deadmans-chest.jpg",
+        date: 2006,
+    },
+    {
+        name: "Rogue One: A Star Wars Story",
+        value: 1058684742,
+        img: "rogue-one.jpg",
+        date: 2016,
+    },
+    {
+        name: "Aladdin",
+        value: 1054304000,
+        img: "aladdin.jpg",
+        date: 2019,
+    },
+    {
+        name: "Pirates of the Caribbean: On Stranger Tides",
+        value: 1046721266,
+        img: "potc-on-stranger-tides.jpg",
+        date: 2011,
+    },
+    {
+        name: "Star Wars: Episode I - The Phantom Menace",
+        value: 1046515409,
+        img: "phantom-menace.jpg",
+        date: 1999,
+    },
+    {
+        name: "Despicable Me 3",
+        value: 1034800131,
+        img: "despicable-me-3.jpg",
+        date: 2017,
+    },
+    {
+        name: "Finding Dory",
+        value: 1029266989,
+        img: "finding-dory.jpg",
+        date: 2016,
+    },
+    {
+        name: "Harry Potter and the Philosopher's Stone",
+        value: 1026407721,
+        img: "philosophers-stone.jpg",
+        date: 2001,
+    }
 ];
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -317,8 +317,8 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const currentUser = document.querySelector("#username").value
-const movieOne = document.querySelector("#movie1")
-const movieTwo = document.querySelector("#movie2")
+const movieOne = document.querySelector("#answer-1")
+const movieTwo = document.querySelector("#answer-2")
 const FILM_1_LS_KEY = "film1";
 const FILM_2_LS_KEY = "film2";
 const USER_USERNAME_LS_KEY = "userUserName";
@@ -326,8 +326,8 @@ const USER_HIGHSCORE_LS_KEY = "userHighScore";
 
 // Here to ensure USER_HIGHSCORE_LS_KEY has a value atleast once so it doesn't return undefined or overwrite itself
 function isHighScoreNumber() {
-    if(!localStorage.getItem(USER_HIGHSCORE_LS_KEY)){
-        localStorage.setItem(USER_HIGHSCORE_LS_KEY,0)
+    if (!localStorage.getItem(USER_HIGHSCORE_LS_KEY)) {
+        localStorage.setItem(USER_HIGHSCORE_LS_KEY, 0)
     }
 }
 
@@ -345,13 +345,13 @@ function runGame(arr) {
 //display two randomly chosen movies from array and stores their index in local storage
 function displayQuiz(number1, number2) {
 
-    movieOne.textContent = test[number1].name;
+    document.querySelector("#movie1").textContent = test[number1].name;
     localStorage.setItem(FILM_1_LS_KEY, number1);
     let imageLoc1 = test[number1].img;
     let imgOne = document.querySelector("#img-1")
     imgOne.innerHTML = `<img src="assets/images/posters/${imageLoc1}" class="poster"></img>`;
 
-    movieTwo.textContent = test[number2].name;
+    document.querySelector("#movie2").textContent = test[number2].name;
     localStorage.setItem(FILM_2_LS_KEY, number2)
     let imageLoc2 = test[number2].img;
     let imgTwo = document.querySelector("#img-2")
@@ -374,7 +374,7 @@ function checkAnswer() {
         runGame(test);
         gameOver();
     }
-        
+
 }
 
 // Check answer for right side button
@@ -394,7 +394,7 @@ function checkAnswer2() {
         runGame(test);
         gameOver();
     }
-    
+
 }
 
 //Increase correct answer counter
@@ -416,15 +416,16 @@ function gameOver() {
     document.querySelector("#restart-game").classList.toggle("display-none")
     let score = document.querySelector("#score").innerText
     let username = document.querySelector("#username").value
-    localStorage.setItem(USER_USERNAME_LS_KEY,username)
-    if(parseInt(localStorage.getItem(USER_HIGHSCORE_LS_KEY)) < score){
-        localStorage.setItem(USER_HIGHSCORE_LS_KEY,score)
+    localStorage.setItem(USER_USERNAME_LS_KEY, username)
+    if (parseInt(localStorage.getItem(USER_HIGHSCORE_LS_KEY)) < score) {
+        localStorage.setItem(USER_HIGHSCORE_LS_KEY, score)
     }
 }
 
 
 //Toggle classes again to return to original state, set correct answer counter to 0, run game again.
-function retryGame(){
+function retryGame() {
+    function displayToggle(){
     document.querySelector("#score").innerText = 0;
     movieOne.classList.toggle("display-none")
     movieTwo.classList.toggle("display-none")
@@ -432,5 +433,7 @@ function retryGame(){
     document.querySelector("#movie-value-2").classList.toggle("display-none")
     document.querySelector(".scores").classList.toggle("display-none")
     document.querySelector("#restart-game").classList.toggle("display-none")
-    runGame(test);
+    }
+    displayToggle();
+    setTimeout(runGame, 4000, test);
 }
